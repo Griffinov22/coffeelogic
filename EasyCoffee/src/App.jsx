2548898483a3a8cd8a78bc427a1d5f980a82d390
Griffinov22 <griffinovermyer@gmail.com>
2022-12-21 20:22:00 -0500

    started navigation

---
+++ EasyCoffee/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./css/App.css";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
---
+++ EasyCoffee/src/components/navigation.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../css/navigation.css";
+
+export default function Navigation(props) {
+  const [isOpen, setIsOpen] = React.useState(true);
+
+  function closeNav() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <>
+      <nav className="nav">
+        <h2 className="nav-title">What are We Brewin'?</h2>
+        <ul className="nav-ul">
+          <li className="nav-item">Pour Over (V60,Chemex)</li>
+          <li className="nav-item">French Press</li>
+          <li className="nav-item">Espresso</li>
+          <li className="nav-item">Cold Brew</li>
+          <li className="nav-item">Tea</li>
+          <li className="nav-item">How did Griffin Create that?</li>
+          <li className="nav-item">Griffin's Obnoxious Coffee Lingo</li>
+        </ul>
+      </nav>
+      <button className="nav-btn" onClick={closeNav}>
+        &gt;
+      </button>
+    </>
+  );
+}
