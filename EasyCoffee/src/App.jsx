1e637cf5c2f051a2bcf24edb465f0b84c0e1378f
Griffinov22 <63012838+Griffinov22@users.noreply.github.com>
2022-12-22 10:29:47 -0500

    Navbar Complete

--- EasyCoffee/src/App.jsx
+++ EasyCoffee/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import "./css/app.css";
 
 function App() {
   return (
--- EasyCoffee/src/components/Navbar.jsx
+++ EasyCoffee/src/components/Navbar.jsx
@@ -1,15 +1,34 @@
+import React from "react";
+import "../css/nav.css";
+
 export default function Navbar() {
+  const [navOpen, setNavOpen] = React.useState(false);
+  console.log(navOpen);
+
+  const btnStyle = {
+    left: "-26px",
+  };
+
   return (
-    <nav>
-      <h2 className="nav-title">What's Brewin'?</h2>
-      <ul className="nav-lst">
-        <li className="list-item">Pour Over Coffee</li>
-        <li className="list-item">French Press</li>
-        <li className="list-item">Espresso</li>
-        <li className="list-item">Tea</li>
-        <li className="list-item">Griffin's Crazy Coffee Terms</li>
-        <li className="list-item">How did Griffin Make That Drink?</li>
-      </ul>
-    </nav>
+    <>
+      <nav className={`nav ${navOpen ? "left" : ""}`}>
+        <h2 className="nav-title">What's Brewin'?</h2>
+        <ul className="nav-lst">
+          <li className="list-item">Pour Over Coffee</li>
+          <li className="list-item">French Press</li>
+          <li className="list-item">Espresso</li>
+          <li className="list-item">Tea</li>
+          <li className="list-item">Griffin's Crazy Coffee Terms</li>
+          <li className="list-item">How did Griffin Make That Drink?</li>
+        </ul>
+      </nav>
+      <button
+        className={`nav-btn`}
+        style={navOpen ? btnStyle : {}}
+        onClick={() => setNavOpen(!navOpen)}
+      >
+        &gt;
+      </button>
+    </>
   );
 }
