2c8b6cc6ceba51bbd23ba7f6cfce95758ed73d9a
Griffinov22 <63012838+Griffinov22@users.noreply.github.com>
2022-12-22 09:26:10 -0500

    I really need to take the time to learn git......

---
+++ EasyCoffee/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2 className="nav-title">What's Brewin'?</h2>
+      <ul className="nav-lst">
+        <li className="list-item">Pour Over Coffee</li>
+        <li className="list-item">French Press</li>
+        <li className="list-item">Espresso</li>
+        <li className="list-item">Tea</li>
+        <li className="list-item">Griffin's Crazy Coffee Terms</li>
+        <li className="list-item">How did Griffin Make That Drink?</li>
+      </ul>
+    </nav>
+  );
+}
