ed2f5e29948b1f0330ccf47d8713dd36fdcb89f3
Griffinov22 <63012838+Griffinov22@users.noreply.github.com>
2022-12-28 22:44:35 -0500

    created homepage

--- EasyCoffee/src/components/pages/homePage.jsx
+++ EasyCoffee/src/components/pages/homePage.jsx
@@ -1,7 +1,43 @@
+import "../../css/homePage.css";
+import latteArt1 from "/latte-art.gif";
+import latteArt2 from "/latte-art2.gif";
+import React from "react";
+
 export default function HomePage() {
+  const headerPhoto = Math.random() > 0.5 ? latteArt1 : latteArt2;
+  const welcomeText = React.useRef(null);
+
+  const observer = new IntersectionObserver(
+    function (entries, observer) {
+      const [entry] = entries;
+      if (entry.isIntersecting) {
+        welcomeText.current.style.opacity = "1";
+        welcomeText.current.style.marginTop = "0px";
+      }
+    },
+    { threshold: "1.0", rootMargin: "0px" }
+  );
+
+  React.useEffect(() => {
+    observer.observe(welcomeText.current);
+  }, []);
+
   return (
-    <div>
-      <h1>WELCOME TO THE HOME PAGE</h1>
-    </div>
+    <>
+      <header>
+        <img className="header-pic" src={headerPhoto} alt="" />
+        <h1 className="title">Easy Coffee</h1>
+        <div className="triangle" id="left-triangle"></div>
+        <div className="triangle" id="right-triangle"></div>
+      </header>
+      <main className="main">
+        <div className="welcome-text" ref={welcomeText}>
+          <h2>
+            Welcome to <span>Coffee</span> made <span>Easy</span>
+          </h2>
+          <h3>With Griffin Overmyer</h3>
+        </div>
+      </main>
+    </>
   );
 }
